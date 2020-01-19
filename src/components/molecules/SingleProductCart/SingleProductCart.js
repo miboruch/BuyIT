38973362087f70c484bb8e5a2283ef53ba62dd34
Michał Boruch <miboruch@gmail.com>
2020-01-19 13:59:02 +0100

    Real delete - after clicking, product is being deleted from database

--- src/components/molecules/SingleProductCart/SingleProductCart.js
+++ src/components/molecules/SingleProductCart/SingleProductCart.js
@@ -6,16 +6,29 @@ import { Link } from 'react-router-dom';
 import { animated } from 'react-spring';
 import { ReactSVG } from 'react-svg';
 import deleteIcon from '../../../assets/icons/delete.svg';
+import { removeProduct } from '../../../actions/productAction';
 
-const StyledLink = styled(animated(Link))`
-  width: 48%;
-  margin-bottom: 2rem;
-  overflow: visible;
-`;
+// const StyledLinkWrapper = styled(Link)`
+//   width: 48%;
+//   margin-bottom: 2rem;
+//   overflow: visible;
+//
+//   ${({ theme }) => theme.mq.tablet} {
+//     display: none;
+//   }
+// `;
 
 const StyledWrapper = styled.div`
-  height: 300px;
   position: relative;
+  margin-bottom: 4rem;
+  width: 100%;
+
+  ${({ theme }) => theme.mq.tabletS} {
+    width: 48%;
+    margin-bottom: 2rem;
+    overflow: visible;
+    display: block;
+  }
 `;
 
 const StyledImage = styled.img`
@@ -45,6 +58,12 @@ const StyledIcon = styled(ReactSVG)`
   width: 30px;
   height: 30px;
   z-index: 100;
+  cursor: pointer;
+  display: none;
+
+  ${({ theme }) => theme.mq.tablet} {
+    display: block;
+  }
   //transform: translate(-50%, -50%);
 `;
 
@@ -63,23 +82,74 @@ const StyledUserName = styled(Paragraph)`
   font-size: 10px;
 `;
 
-const SingleProductCart = ({ image, name, price, id, userLogin, style, userID, productUserID }) => {
+const StyledLink = styled(Link)`
+  width: 150px;
+`;
+
+const OpenProduct = styled(Paragraph)`
+  position: relative;
+  padding: 1rem 0;
+  font-weight: bold;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  display: block;
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    width: 0;
+    height: 2px;
+    background: #fff;
+    transition: all 1s cubic-bezier(0.66, 0.24, 0, 0.82);
+  }
+  &:hover::after {
+    width: 150px;
+  }
+  // ${({ theme }) => theme.mq.standard} {
+  //   display: block;
+  // }
+`;
+
+const SingleProductCart = ({
+  image,
+  name,
+  price,
+  id,
+  userLogin,
+  style,
+  userID,
+  productUserID,
+  token,
+  removeProduct
+}) => {
   return (
-    <StyledLink to={`/product/${id}`} style={style}>
-      <StyledWrapper>
-        {userID === productUserID ? <StyledIcon src={deleteIcon} /> : null}
-        {/*<StyledCover>Details</StyledCover>*/}
-        {image ? <StyledImage src={image} /> : <p>no image</p>}
-        <StyledNameParagraph medium>{name}</StyledNameParagraph>
-        <StyledPrice>{price}$</StyledPrice>
-        <StyledUserName>User: {userLogin}</StyledUserName>
-      </StyledWrapper>
-    </StyledLink>
+    <StyledWrapper>
+      {userID === productUserID ? (
+        <StyledIcon src={deleteIcon} onClick={() => removeProduct(token, id)} />
+      ) : null}
+      {/*<StyledCover>Details</StyledCover>*/}
+      {image ? <StyledImage src={image} /> : <p>no image</p>}
+      <StyledNameParagraph medium>{name}</StyledNameParagraph>
+      <StyledPrice>{price}$</StyledPrice>
+      <StyledUserName>User: {userLogin}</StyledUserName>
+      <Link to={`/product/${id}`} style={style}>
+        <OpenProduct small>Open product</OpenProduct>
+      </Link>
+    </StyledWrapper>
   );
 };
 
-const mapStateToProps = ({ authenticationReducer: { userID } }) => {
-  return { userID };
+const mapStateToProps = ({ authenticationReducer: { userID, token } }) => {
+  return { userID, token };
 };
 
-export default connect(mapStateToProps)(SingleProductCart);
+const mapDispatchToProps = dispatch => {
+  return {
+    removeProduct: (token, productID) => dispatch(removeProduct(token, productID))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProductCart);
